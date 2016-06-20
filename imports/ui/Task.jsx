c87257bc0f67f328e0b82dd760219432429c90a5
Philip Orchard <philiporchard@gmail.com>
2016-06-20 04:25:13 -0700

    Add publication for tasks

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -31,7 +31,7 @@ export default class Task extends Component {
         />
 
         <span className="text">
-          {this.props.task.text} <strong> - {this.props.task.username}</strong>
+          {this.props.task.text} <strong>  {this.props.task.username}</strong>
         </span>
       </li>
     );
