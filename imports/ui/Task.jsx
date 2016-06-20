4fe2cdd6fa1ed3b2d3a711e6593aafff1d5f1d61
Philip Orchard <philiporchard@gmail.com>
2016-06-20 04:41:12 -0700

    Add a new prop type for Task component

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -37,3 +37,10 @@ export default class Task extends Component {
     );
   }
 }
+
+Task.propTypes = {
+  // This component gets the task to display through a React prop.
+  // We can use propTypes to indicate it is required
+  task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired,
+};
