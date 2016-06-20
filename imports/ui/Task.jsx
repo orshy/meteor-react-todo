c8bbf8c07cf15e34e9ffa26aeca346491e56a26e
Philip Orchard <philiporchard@gmail.com>
2016-06-20 04:44:39 -0700

    Add private button, shown only to owner

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -30,6 +30,12 @@ export default class Task extends Component {
           onClick={this.toggleChecked.bind(this)}
         />
 
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
+
         <span className="text">
           {this.props.task.text} <strong>  {this.props.task.username}</strong>
         </span>
