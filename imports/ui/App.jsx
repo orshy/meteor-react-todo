b2a4f15bf8e3352216675effb564d17da8a78ab7
Philip Orchard <philiporchard@gmail.com>
2016-06-20 02:35:08 -0700

    Add toggleHideCompleted handler to App

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,14 @@ import { Tasks } from '../api/tasks.js';
 
   // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   handleSubmit(event) {
      event.preventDefault();
 
@@ -21,7 +29,13 @@ class App extends Component {
 
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
-    }
+  }
+
+    toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
 
     renderTasks() {
       return this.props.tasks.map((task) => (
@@ -34,6 +48,16 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
